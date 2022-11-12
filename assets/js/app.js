ae0209340718b540f60501eaa42f028766fdfaa7
RonaldGuilhermePDS <ronaldguilhermepds@gmail.com>
2022-11-12 00:05:15 -0500

    fix: change alpine js version to ^2.8.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,15 +29,17 @@ let liveSocket = new LiveSocket("/live", Socket, {
   params: { _csrf_token: csrfToken },
   dom: {
     onBeforeElUpdated(from, to) {
-      if (from.__x) { Alpine.clone(from.__x, to) }
+        if (from.__x) {
+          Alpine.clone(from.__x, to);
+        }
     }
   }
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .2)"})
+window.addEventListener("phx:page-loading-start", () => topbar.show())
+window.addEventListener("phx:page-loading-stop", () => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
