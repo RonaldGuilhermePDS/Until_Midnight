92b1f8e5047cd11e0d61adc52db89d5b505cd192
RonaldGuilhermePDS <ronaldguilhermepds@gmail.com>
2022-11-12 11:06:40 -0500

    feat: add sign in and sign up structure

--- lib/until_midnight_web/live/page_live.ex
+++ lib/until_midnight_web/live/page_live.ex
@@ -1,7 +1,34 @@
 defmodule UntilMidnightWeb.PageLive do
   use UntilMidnightWeb, :live_view
 
+  alias UntilMidnight.Accounts
+
+  alias UntilMidnight.Accounts.User
+
+  @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    changeset = Accounts.change_user_registration(%User{})
+
+    {:ok,
+      socket
+      |> assign(changeset: changeset)
+      |> assign(trigger_submit: false)
+    }
+  end
+
+  @impl true
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset =
+      %User{}
+      |> User.registration_changeset(user_params)
+      |> Map.put(:action, :validate)
+
+      :timer.sleep(8000)
+
+      {:noreply, socket |> assign(changeset: changeset)}
+  end
+
+  def handle_event("save", _, socket) do
+    {:noreply, assign(socket, trigger_submit: true)}
   end
 end
--- lib/until_midnight_web/views/error_helpers.ex
+++ lib/until_midnight_web/views/error_helpers.ex
@@ -8,11 +8,11 @@ defmodule UntilMidnightWeb.ErrorHelpers do
   @doc """
   Generates tag for inlined form input errors.
   """
-  def error_tag(form, field) do
+  def error_tag(form, field, class \\ [class: "invalid-feedback"]) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
-        phx_feedback_for: input_name(form, field)
+        class: Keyword.get(class, :class),
+        phx_feedback_for: input_id(form, field)
       )
     end)
   end
